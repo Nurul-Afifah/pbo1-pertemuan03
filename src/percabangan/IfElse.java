6782e529f71c73e812274b4e030b42a0632db986
NurulAfifah <nurullafifahh1112@gmail.com>
2020-04-29 21:44:10 +0700

    false

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangdiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangdiDompet = 53000;
 
         if (uangdiDompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
         }else{
-            System.out.println("Uang cukup, selamat bisa jajan");
+            System.out.println("Uang cukup, Alhamdulillah bisa jajan");
         }
     }
 }
