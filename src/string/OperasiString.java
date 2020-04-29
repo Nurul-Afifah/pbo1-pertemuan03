b4bb049ba60f72388879866d2daa2d7444b24bc1
NurulAfifah <nurullafifahh1112@gmail.com>
2020-04-29 19:46:45 +0700

    Membuat String dari array Char

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -5,5 +5,9 @@ public class OperasiString {
         String kota = "Tapin";
 
         System.out.println(kota);
+
+        char[] nurulChar = { 'N','U','R','U','L' };
+        String nurulString = new String(nurulChar);
+        System.out.println(nurulString);
     }
 }
